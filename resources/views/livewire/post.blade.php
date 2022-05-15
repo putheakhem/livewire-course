e4d2d8138dde1c0f8485ad0fa89d43ff4e15f9cc
putheakhem <puthea.khem@gmail.com>
2022-05-15 15:00:26 +0700

    Setup Tailwind CSS

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <input type="text" wire:model.lazy="title">
-    <h1>My Post Title: {{ $title }}</h1>
+    <input class="border border-red-500 rounded-sm hover:border-yellow-400" type="text" wire:model.lazy="title">
+    <h1 class="text-3xl font-bold underline text-green-500">My Post Title: {{ $title }}</h1>
 </div>
